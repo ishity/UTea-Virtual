e1ceafca5d354c0eb82495040d1d19de64a76dd8
ishity__ <88872183+ishity@users.noreply.github.com>
2023-03-04 17:50:36 +0900

    Update slide.js
    delete comments

--- utea_virtual/js/slide.js
+++ utea_virtual/js/slide.js
@@ -5,36 +5,14 @@ const text = ["①右上の「GUIDE」というボタンを押すと操作方法
 let num = -1;
 let time = 0;
 
-// function start() {
-//     setInterval(slideshow_timer,2000); 
-// }
-
-// function picChange(n){
-//     document.getElementById("mypic").src = pics_src[n];
-//     document.getElementById("page").textContent = text[n];
-// }
-
-// function slideshow_timer(){
-//     if (num == 1){
-//         num = 0;
-//     } 
-//     else {
-//         num ++;
-//     }
-//     setTimeout("picChange(num)", 2000);
-//     // setInterval(slideshow_timer,2500); 
-// }
-
 function picChange(){
     if (num == 1){
         num = 0;
     }else {
         num ++;
     }
-    // setTimeout(picChange(num), 2500);
     document.getElementById("mypic").src = pics_src[num];
     document.getElementById("page").textContent = text[num];
-    // document.getElementById("mypic").classList.add('is-not-show');
 }
 
 function slideshow_timer(){
@@ -43,9 +21,7 @@ function slideshow_timer(){
     }else {
         num ++;
     }
-    // picChange(num);
     setTimeout(picChange(num), 2500);
-    // setInterval(slideshow_timer,2500); 
 }
 
 // ---------- スライドの画像変換ここまで ----------
@@ -63,18 +39,8 @@ setTimeout(load_effect, 600); // 600ミリ秒経過後に実行
 
 // ---------- 実行 ----------
 
-// 画面をスクロールをしたら動かしたい場合の記述
 setInterval(() => {
     picChange();
 }, 2500);
-
-// $(window).on('load', function () {
-// 	setInterval(slideshow_timer(),2500); //スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
-// });
-
-// ページが読み込まれたらすぐに動かしたい場合の記述
-// $(window).on('load', function () {
-// 	setInterval(slideshow_timer(),2500); //スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
-// });
 
 // ---------- 実行ここまで ----------
